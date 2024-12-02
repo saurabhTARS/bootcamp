b8346147956cc25f3c0e973f00ec0e4a08ad61e7
saurabhTARS <saurabhprasad12@gmail.com>
2024-12-02 21:00:04 +0530

    02-12-2024 : codeforces java implementation

---
+++ cf/java/bit.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class bit
+{
+    public static void main(String[] args)
+    {
+        Scanner myObj = new Scanner(System.in);
+        int x = 0;
+
+        String userName = myObj.nextLine();
+        try 
+        {
+            int numRows = Integer.parseInt(userName);
+            // System.out.println("The input was : " + numRows);
+
+            for(int i = 0; i < numRows; i++)
+            {
+                String operation = myObj.nextLine();
+
+                if(operation.contains("++"))
+                    x++;
+                else if(operation.contains("--"))
+                    x--;
+            }
+
+            System.out.println(x);
+
+        }
+        catch(Exception e)
+        {
+            // System.out.println("The input is not an integer : " + userName);
+            System.exit(0);
+        }
+
+    }
+}
---
+++ cf/java/chew.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class chew
+{
+    public static void main(String[] args)
+    {
+        Scanner myObj = new Scanner(System.in);
+        String nums = myObj.nextLine();
+        StringBuilder result = new StringBuilder("");
+
+        for(char num : nums.toCharArray())
+        {
+            int digit = Character.getNumericValue(num);
+            
+            if((9-digit) < digit)
+            {
+                result.append((9-digit));
+            }
+            else
+            {
+                result.append(digit);
+            }
+
+        }
+        if(Character.getNumericValue(result.charAt(0)) == 0)
+            result.setCharAt(0, nums.charAt(0));
+        System.out.println(result);
+    }
+}
