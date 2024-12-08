2147d80501fc4f2efbab36f645deaab837e6d719
saurabhTARS <saurabhprasad12@gmail.com>
2024-12-08 23:32:13 +0530

    08-12-2024 : three problems completed with new knowledge of TreeMap

---
+++ cf/java/newYear.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class newYear {
+    public static void main(String[] args)
+    {
+        Scanner myObj = new Scanner(System.in);
+        String input = myObj.nextLine();
+        String[] words = input.split("\\s");
+        int[] nums = new int[3];
+        int i = 0;
+
+        for(String word : words)
+        {
+            nums[i++] = Integer.parseInt(word);
+        }
+        i = 0;
+        int result = Integer.MAX_VALUE;
+
+        while(i < 3)
+        {
+            if(i == 0)
+            {
+                result = Math.min(Math.abs(nums[0] - nums[1]) + Math.abs(nums[0] - nums[2]), result);
+                // System.out.println(result);
+            }
+            if(i == 1)
+            {
+                result = Math.min(Math.abs(nums[0] - nums[1]) + Math.abs(nums[1] - nums[2]), result);
+                // System.out.println(result);
+            }
+            if(i == 2)
+            {
+                result = Math.min(Math.abs(nums[0] - nums[2]) + Math.abs(nums[1] - nums[2]), result);
+                // System.out.println(result);
+            }
+            i++;
+        }
+
+        System.out.println(result);
+        myObj.close();
+    }
+}
